b0d06739645e0f57cb55278f740508d915f54dbb
Tanaya <tanaya.babtiwale@haptik.ai>
2021-05-23 15:32:14 +0530

    New format PAN + bug fixes

--- cards/aadhaar.py
+++ cards/aadhaar.py
@@ -17,31 +17,37 @@ class Aadhaar:
         self.img_data = img_data
         self.processed_data = ""
 
-    def _check_alphabet_patterns(self,str):
+    def _check_alphabet_patterns(self, str):
 
         vowels_consonants = '[^\S\r\n].*?(([aeiou]{3,})|([^aeiou\s0-9]{4,})).*?[^\S\r\n]'
-        text = re.sub(vowels_consonants, " ", str, re.IGNORECASE)
+        print(re.findall(vowels_consonants, str, re.IGNORECASE))
+        text = re.sub(vowels_consonants, " ", str, flags=re.IGNORECASE)
 
+        print(f'3.1: {text}')
         anti_title_case = "[^\S\r\n][A-Z]*[a-z]+[A-Z][a-zA-Z]*?[^\S\r\n]"
         text = re.sub(anti_title_case, " ", text)
+        print(f'3.2: {text}')
 
         return text
 
-
     def preprocess_data(self):
-        text = re.sub('[^-a-zA-Z0-9/\s]',' ',self.img_data)
-        text = re.sub('[^\S\r\n]',' ',text)
-        text = re.sub('\n+','\n',text)
+        text = re.sub('[^-a-zA-Z0-9/\s]', ' ', self.img_data)
+        print(f'1: {text}')
+        text = re.sub('[^\S\r\n]', ' ', text)
+        print(f'2: {text}')
+        text = re.sub('\n+', '\n', text)
+        print(f'3: {text}')
         text = self._check_alphabet_patterns(text)
+        print(f'4: {text}')
         lines = []
         for line in text.split('\n'):
             line = line.strip()
-            if len(line)>3:
+            if len(line) > 3:
                 lines.append(line)
         self.processed_data = '\n'.join(lines)
 
         print(self.img_data)
-        print('+'*25)
+        print('+' * 25)
         print(self.processed_data)
 
     def front_aadhaar_data(self) -> dict:
@@ -57,15 +63,16 @@ class Aadhaar:
     def get_aadhaar_id(self):
         aadhaar_id = None
         try:
-            aadhaar_id = re.search('[0-9]{4} [0-9]{4} [0-9]{4}', self.processed_data).group()
+            aadhaar_id = re.search('(?<!([0-9] ))[0-9]{4} [0-9]{4} [0-9]{4}(?!( [0-9]))', self.processed_data).group()
         except Exception as error:
             print('Error in parsing aadhaar id::', error)
         return aadhaar_id
 
     def get_name(self):
         try:
-            print(re.findall('(?:(?:^|\s)[A-Z][a-z]*)+',self.processed_data))
-            return max(re.findall('(?:(?:^|\s)[A-Z][a-z]*)+',self.processed_data), key=len)
+            text = re.sub(".*government|india", "", self.processed_data, flags=re.IGNORECASE | re.DOTALL)
+            print(re.findall('(?:(?:^|\s)[A-Z][a-z]*)+', text))
+            return re.sub('(^\s)|(\s$)', '', max(re.findall('(?:(?:^|\s)[A-Z][a-z]*)+', text), key=len))
         except Exception as e:
             print(f'Error in Name Detection: {e}')
         return None
@@ -74,18 +81,18 @@ class Aadhaar:
         lines = self.processed_data.split('\n')
         dob = defaultdict()
         for line in lines:
-            if re.search('(year)|(yob)',line,re.IGNORECASE):
+            if re.search('(year)|(yob)', line, re.IGNORECASE):
                 try:
                     dob['year'] = re.search('[0-9]{4}', line)
                 except Exception as e:
                     print(f'Error in YOB: {e}')
 
-            elif re.search('(date )|(dob)|(d0b)',line,re.IGNORECASE):
+            elif re.search('(date )|(dob)|(d0b)', line, re.IGNORECASE):
                 print(f'Found DOB line: {line}')
                 try:
                     dobstr = re.search(r'[0-9]{2}/[0-9]{2}/[0-9]{4}', line).group()
                     dobstr = dobstr.split('/')
-                    if int(dobstr[1])<=12:
+                    if int(dobstr[1]) <= 12:
                         dob['day'] = dobstr[0]
                         dob['month'] = dobstr[1]
                     else:
@@ -100,12 +107,11 @@ class Aadhaar:
         for line in self.processed_data.split('\n'):
             if re.search('[male]{4}', line, re.IGNORECASE):
                 print(f'Found Gender line: {line}')
-                try:
-                    return re.search('female', line, re.IGNORECASE).group()
-                except Exception as e:
-                    try:
-                        return re.search('male', line, re.IGNORECASE).group()
-                    except Exception as e:
-                        print('Gender Not Found')
+                if re.search('female|emal', line, re.IGNORECASE):
+                    return "FEMALE"
+                elif re.search('male', line, re.IGNORECASE):
+                    return "MALE"
+                else:
+                    print('Gender Not Found')
 
         return None
--- cards/pan.py
+++ cards/pan.py
@@ -14,39 +14,39 @@ class Pan:
     def __init__(self, img_data) -> None:
         self.img_data = img_data
         self.processed_data = ""
-        self.clarity_flag=False
+        self.clarity_flag = False
 
-    def _check_alphabet_patterns(self,str):
+    def _check_alphabet_patterns(self, str):
         vowels_consonants = '[^\S\r\n].*?(([aeiou]{3,})|([^aeiou\s0-9]{4,})).*?[^\S\r\n]'
         text = re.sub(vowels_consonants, " ", str, re.IGNORECASE)
         anti_title_case = "[^\S\r\n][A-Z]*[a-z]+[A-Z][a-zA-Z]*?[^\S\r\n]"
         text = re.sub(anti_title_case, " ", text)
         return text
 
-
     def preprocess_data(self):
-        text = re.sub('[^-a-zA-Z0-9/\s]',' ',self.img_data)
-        text = re.sub('[^\S\r\n]',' ',text)
-        text = re.sub('\n+','\n',text)
+        text = re.sub('[^-a-zA-Z0-9/\s]', ' ', self.img_data)
+        text = re.sub('[^\S\r\n]', ' ', text)
+        text = re.sub('\n+', '\n', text)
         text = self._check_alphabet_patterns(text)
         lines = []
         for line in text.split('\n'):
             line = line.strip()
-            if len(line)>3:
+            if len(line) > 3:
                 lines.append(line)
         self.processed_data = '\n'.join(lines)
 
         print(self.img_data)
-        print('+'*25)
+        print('+' * 25)
         print(self.processed_data)
+
     def get_pan_details(self) -> dict:
         self.preprocess_data()
         names = self.get_names()
         pan_data = {
             'pan_no': self.get_pan_number(),
             'dob': self.get_dob(),
-            'name': names.get('holder_name',None),
-            'father_name' : names.get('father_name',None)
+            'name': names.get('holder_name', None),
+            'father_name': names.get('father_name', None)
         }
         if not self.clarity_flag:
             pan_data['error'] = "Image not clear enough"
@@ -57,32 +57,55 @@ class Pan:
         for line in self.processed_data.split('\n'):
             if self.clarity_flag:
                 try:
-                    return re.search('[A-Z]{5}[0-9]{4}[A-Z]',line).group()
+                    return re.search('[A-Z]{5}[0-9]{4}[A-Z]', line).group()
                 except Exception:
                     self.clarity_flag = False
                     print('PAN Number not found')
-            if re.search('Permanent Account Number',line):
+            if re.search('Permanent Account Number', line):
                 self.clarity_flag = True
         return pan_no
 
     def get_names(self):
         prev_line = ''
         names = defaultdict()
-        for line in self.processed_data.split('\n'):
-            if re.search('income|tax|department|government|india|govt',prev_line, re.IGNORECASE):
-                try:
-                    names['holder_name'] = max(re.findall('[A-Z ]+',line), key=len)
-                    prev_line = "name_detected"
-                    continue
-                except Exception as e:
-                    print(f'Error in Name Detection: {e}')
-                return None
-            elif "name_detected" in prev_line:
-                try:
-                    names['father_name'] = max(re.findall('[A-Z ]+',line), key=len)
-                except Exception as e:
-                    print(f'Error in Father Name Detection: {e}')
+        set_name = False
+        set_fname = False
+        lines = self.processed_data.split('\n')
+        for line in lines:
+            if re.search('(name)|(n.me)|(na.e)', prev_line, re.IGNORECASE):
+                if re.search('father', prev_line, re.IGNORECASE) and not set_fname:
+                    try:
+                        names['father_name'] = max(re.findall('[A-Z ]+', line), key=len)
+                        set_fname = True
+                        print(f'Father line : {line} ')
+                    except Exception as e:
+                        print(f'Error in Father Name Detection: {e}')
+                elif not set_name:
+                    try:
+                        names['holder_name'] = max(re.findall('[A-Z ]+', line), key=len)
+                        set_name = True
+                        print(f'Name line : {line} // {prev_line}')
+                    except Exception as e:
+                        print(f'Error in Name Detection: {e}')
             prev_line = line
+
+        if not set_name and not set_fname:
+            for line in lines:
+                if re.search('income|tax|department|government|india|govt', prev_line, re.IGNORECASE):
+                    try:
+                        names['holder_name'] = max(re.findall('[A-Z ]+', line), key=len)
+                        prev_line = "name_detected"
+                        continue
+                    except Exception as e:
+                        print(f'Error in Name Detection: {e}')
+                    return None
+                elif "name_detected" in prev_line:
+                    try:
+                        names['father_name'] = max(re.findall('[A-Z ]+', line), key=len)
+                    except Exception as e:
+                        print(f'Error in Father Name Detection: {e}')
+                prev_line = line
+
         return names
 
     def get_dob(self):
@@ -90,7 +113,7 @@ class Pan:
         try:
             dobstr = re.search(r'[0-9]{2}/[0-9]{2}/[0-9]{4}', self.processed_data).group()
             dobstr = dobstr.split('/')
-            if int(dobstr[1])<=12:
+            if int(dobstr[1]) <= 12:
                 dob['day'] = dobstr[0]
                 dob['month'] = dobstr[1]
             else:
